67e1503fb20fd249dbbe6677ad4d7a7fc3c62910
Kai Lee <kai.lee@sendbird.com>
2024-11-14 18:47:23 +0900

    Initial Commit

---
+++ FourWayRoulette/FourWayRouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  FourWayRouletteApp.swift
+//  FourWayRoulette
+//
+//  Created by Kai Lee on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct FourWayRouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
